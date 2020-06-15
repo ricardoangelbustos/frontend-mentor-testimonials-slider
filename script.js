d553421a36017daade785415580662b9602e3828
Ricardo Bustos <ricardobustos14@outlook.com>
2020-06-14 22:57:42 -0300

    Solucionada carga del texto erronea

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function changeText() {
   var text = document.getElementById("text");
-  if (text.innerHTML == "“ I’ve been interested in coding for a while <br> but never taken the jump, until now. I <br> couldn’t recommend this course enough. <br> I’m now in the job of my dreams and so <br> excited about the future. ”") {
+  if (text.innerHTML.match("“ I’ve")) {
     text.innerHTML = "“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”";
   } else {
     text.innerHTML = "“ I’ve been interested in coding for a while <br> but never taken the jump, until now. I <br> couldn’t recommend this course enough. <br> I’m now in the job of my dreams and so <br> excited about the future. ”";
